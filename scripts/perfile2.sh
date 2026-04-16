1e72b8e237da6b3b314a6e124c00a7e0019e8195
James Noble <kjx@ecs.vuw.ac.nz>
2026-04-16 03:37:42 +0200

    add scripts for new typesystem

--- scripts/perfile2.sh
+++ scripts/perfile2.sh
@@ -1,1 +1,2 @@
-for i in [A-W]*.dfy; do echo; echo; echo ==========;  echo $i $i $i; date; time nlately verify $i --verification-time-limit=10 --isolate-assertions --cores 8 | tee logs/$(date +%b%d)-$i.txt; date; echo; done;  
+PREFIX=${1:-$(date +%b%d)}
+for i in [A-W]*.dfy; do echo; echo; echo ==========;  echo $i $i $i; date; time nlately verify $i --verification-time-limit=10 --isolate-assertions --cores 8 | tee logs/$PREFIX-$i.txt; date; echo; done;  
--- scripts/perxlone2.sh
+++ scripts/perxlone2.sh
@@ -1,2 +1,3 @@
+PREFIX=${1:-$(date +%b%d)}
 for i in Xlone_Set_Field Xlone_Via_Map Xlone_All_Owners  Xlone_Clone_Clone Xlone_Field_Map   Xlone_All_Fields
-  do echo ====================; echo $i;  echo $i;  echo $i; date; time lately verify Xlone.dfy --verification-time-limit=10 --isolate-assertions  --cores=8  --filter-symbol=$i  --progress=Symbol | tee logs/$(date +%b%d)-$i.dfy.txt; date;  done; 
+  do echo ====================; echo $i;  echo $i;  echo $i; date; time lately verify Xlone.dfy --verification-time-limit=10 --isolate-assertions  --cores=8  --filter-symbol=$i  --progress=Symbol | tee logs/$PREFIX-$i.dfy.txt; date;  done; 
