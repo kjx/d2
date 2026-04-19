5f8f433afbb0b7c8962c27495a6a4b1bc089a37b
James Noble <kjx@ecs.vuw.ac.nz>
2026-04-19 12:39:10 +0200

    perfile 2 down to 6 cores

--- scripts/perfile2.sh
+++ scripts/perfile2.sh
@@ -1,3 +1,3 @@
 PREFIX=${1:-$(date +%b%d)}
 shift;
-for i in $*; do echo; echo; echo ==========;  echo $i $i $i; date; time nlately verify $i --verification-time-limit=10 --isolate-assertions --cores 8 | tee logs/$PREFIX-$i.txt; date; echo; done;  
+for i in $*; do echo; echo; echo ==========;  echo $i $i $i; date; time nlately verify $i --verification-time-limit=10 --isolate-assertions --cores 6 | tee logs/$PREFIX-$i.txt; date; echo; done;  
