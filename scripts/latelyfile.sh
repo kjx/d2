19c6e408e8282103990d85f046ba45d5802dd64a
James Noble <kjx@ecs.vuw.ac.nz>
2026-09-16 18:55:50 +0200

    tagged name on script

--- scripts/latelyfile.sh
+++ scripts/latelyfile.sh
@@ -3,4 +3,4 @@
 shopt -s expand_aliases
 
 PREFIX=$(date +%b%d)
-for i in $*; do echo; echo; echo ==========;  echo $i $i $i; date; time nlately verify $i --verification-time-limit=10 --isolate-assertions --cores 6 | tee logs/$PREFIX-$i.txt; echo done $i $i $i; date; echo; done;  
+for i in $*; do echo; echo; echo ==========;  echo latelyfile $i $i $i; date; time nlately verify $i --verification-time-limit=10 --isolate-assertions --cores 6 | tee logs/$PREFIX-$i.txt; echo done latelyfile $i $i $i; date; echo; done;  
--- scripts/perfile.sh
+++ scripts/perfile.sh
@@ -3,4 +3,4 @@
 shopt -s expand_aliases
 
 PREFIX=$(date +%b%d)
-for i in $*; do echo; echo; echo ==========;  echo $i $i $i; date; time nnightly verify $i --verification-time-limit=10 --isolate-assertions --cores 6 | tee logs/$PREFIX-$i.txt; echo done $i $i $i; date; echo; done;  
+for i in $*; do echo; echo; echo ==========;  echo perfile $i $i $i; date; time nnightly verify $i --verification-time-limit=10 --isolate-assertions --cores 6 | tee logs/$PREFIX-$i.txt; echo done perfile $i $i $i; date; echo; done;  
